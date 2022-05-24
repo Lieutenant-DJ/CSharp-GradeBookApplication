0e25b01ae5d8c9d3912c2cd5bfc78a58579168ee
Lieutenant-DJ <67519347+Lieutenant-DJ@users.noreply.github.com>
2022-05-24 15:17:20 -0400

    3_RankedGradeBook
    Added Override GetLetterGrade Method to rankedGrade

--- GradeBook/GradeBooks/RankedGradeBook.cs
+++ GradeBook/GradeBooks/RankedGradeBook.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using GradeBook.Enums;
+using System.Linq;
 
 namespace GradeBook.GradeBooks
 {
@@ -10,6 +11,41 @@ namespace GradeBook.GradeBooks
         public RankedGradeBook(string name) : base(name)
         {
             Type = GradeBookType.Ranked;
+        }
+
+
+        public override char GetLetterGrade(double averageGrade)
+        {
+            if (BelowMinAcceptableStudentAmount())
+            {
+                throw new InvalidOperationException("Insufficient number of students; 5 student minimum required.");
+            }
+
+            var higherGrades = Students.Count(grades => grades.AverageGrade >= averageGrade);
+            var percentile = (Students.Count - higherGrades) / Students.Count * 100;
+
+            switch (averageGrade)
+            {
+                case var d when d >= 80.0:
+                    return 'A';
+
+                case var d when d >= 60.0:
+                    return 'B';
+
+                case var d when d >= 40.0:
+                    return 'C';
+
+                case var d when d >= 20.0:
+                    return 'D';
+
+                default:
+                    return 'F';
+            }
+        }
+
+        public bool BelowMinAcceptableStudentAmount()
+        {
+            return (Students.Count < 5);
         }
     }
 }
